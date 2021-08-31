9f72017577dbba61a140e6b9b36a8b6f9a56b6cb
Oksana991 <60651019+Oksana991@users.noreply.github.com>
2021-08-31 12:30:56 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,7 @@ namespace Mas
         public int age;
         public decimal salary;
         public string dep;
-        
+
         public Empls(string n, int a, decimal s, string d)
         {
             name = n; age = a; salary = s; dep = d;
@@ -22,9 +22,7 @@ namespace Mas
 
         public void DisplayInfo()
         {
-
-
-
+            Console.WriteLine($"Name: {name}  Age: {age} Salary = {salary} Departmrnt {dep} ");
         }
 
     }
@@ -32,11 +30,14 @@ namespace Mas
     {
         public static void Main(string[] args)
         {
-            Empls[] empl = new Empls[3];
-            for (int i = 0; i < empl.Length; i++)
-                
-            {
 
+            int size = 1;
+            Empls[] empl = new Empls[size];
+
+
+            for (int i = 0; i < empl.Length; i++)
+
+            {
                 Console.Write("Enter name employee: ");
                 string n = Console.ReadLine();
                 Console.Write("Enter age employee: ");
@@ -46,107 +47,119 @@ namespace Mas
                 Console.WriteLine("Enter department: ");
                 string d = Console.ReadLine();
                 empl[i] = new Empls(n, a, s, d);
-                //   когда нажать Y, ввод массива закончится
-                Console.Write("if you want to end the array press Y if no press N");
-                if (Console.ReadLine() == "Y")  break;
-                // if   (Console.ReadLine() == "Y");
-                //{ continue; }
+                int count = empl.Length;
+                if (count != 0)
+                {
+                    Array.Resize(ref empl, empl.Length + 1);
+                }//   когда нажать Y, ввод массива закончится
+                Console.Write("if you want to end the array press Y if no press N\n");
+                if (Console.ReadLine() == "Y") break;
 
 
-
-
-
+                // if (count)
+                //  {
+                //      empl = CopyEmployL
+                // }
 
 
             }
+            metka:
+            Console.WriteLine("select an action: " +
+                "\n a = Min salary of employees; " +
+                "\n b = Max salary of employees;" +
+                "\n c = AVR age of employees; " +
+                "\n d = Output to the console of all employees," +
+                "\n e = Output to the console of all employees by department " +
+                "\n f = Search by name"+
+                "\n g = Press g to exit");
+           
 
-            Console.WriteLine("select an action: a = Min salary of employees; \n b = Max salary of employees;\n c = AVR age of employees; \n d = Output to the console of all employees, e= ");
-                string selection = Console.ReadLine();
-
-
-                switch (selection)
-
-                {
-                    case "a":
-
-
-
-                        decimal min = empl[0].salary;
-
-                        for (int t = 0; t < empl.Length; t++)
-                        {
-                            if (min > empl[t].salary)
-                                min = empl[t].salary;
-
-                        }
+            string selection = Console.ReadLine();
+            
 
 
-                        Console.WriteLine($"Min salary ={min}");
+            switch (selection)
 
+            {
+                case "a":
+                    decimal min = empl[0].salary;
 
-
-                        break;
-
-                    case "b":
-
-                        decimal max = empl[0].salary;
-                        for (int t = 0; t < empl.Length; t++)
-                        {
-                            if (max < empl[t].salary)
-                                max = empl[t].salary;
-
-                        }
-
-                        Console.WriteLine($"Max salary = {max}");
-                        break;
-
-                    case "c":
-
-                        int avg = 0;
-                        int agetotal = 0;
-                        for (int t = 0; t < empl.Length; t++)
-                        {
-                            agetotal += empl[t].age;
-                        }
-                        avg = agetotal / empl.Length;
-                        Console.WriteLine($"AVG = {avg}");
-
-                        break;
-
-                     case "d":
-
-
-                    for (int i = 0; i < empl.Length; i++)
+                    for (int t = 0; t < empl.Length; t++)
                     {
-
-                        Console.WriteLine(empl);
-
+                        if (min > empl[t].salary)
+                            min = empl[t].salary;
                     }
-                    Console.WriteLine();
+                    Console.WriteLine($"Min salary ={min}");
 
+                    goto metka;
 
-                    //   Console.WriteLine(empl[n, a, s, d]);
+                    
+                case "b":
+                    decimal max = empl[0].salary;
+                    for (int t = 0; t < empl.Length; t++)
+                    {
+                        if (max < empl[t].salary)
+                            max = empl[t].salary;
+                    }
+                    Console.WriteLine($"Max salary = {max}");
+                    goto metka;
 
-
-                    // break;
-
-
-
-                    // case "e":
-
-
-                    // Console.Write(String.Format("{0,4}", Empls[n, a, s, d]));
+                    
+                case "c":
+                    int avg = 0;
+                    int agetotal = 0;
+                    for (int t = 0; t < empl.Length; t++)
+                    {
+                        agetotal += empl[t].age;
+                    }
+                    avg = agetotal / empl.Length;
+                    Console.WriteLine($"AVG = {avg}");
                     break;
+                case "d":
+                    foreach (Empls e in empl)
+                    {
+                        e.DisplayInfo();
+                    }
+                    goto metka;
+                    
+                case "e":
+
+                    Console.WriteLine($"Name of department ");
+                    var depName = Console.ReadLine();
+                    foreach (Empls e in empl)
+                    {
+                        if (e.dep.Contains(depName))
+                        {
+                            e.DisplayInfo();
+                        }
+                    }
+                    
+                        goto metka;
+                    
+                    
+                case "f":
+
+                    Console.WriteLine("\n\nName of employy:");
+                    var Nameempl = Console.ReadLine();
+                    foreach (Empls e in empl)
+                    {
+                        if (e.dep.Contains(Nameempl))
+                        {
+                            e.DisplayInfo();
+                        }
+                    }
+                    goto metka;
+
+                case "g":
+                    return;
 
 
-                        default:
-                        Console.WriteLine("Wrong latter");
-                        break;
-
-
-                }
-                Console.ReadKey();
-           // }
+                default:
+                    Console.WriteLine("Wrong latter");
+                    goto metka;
+                    
+            }
+            Console.ReadKey();
         }
     }
 }
